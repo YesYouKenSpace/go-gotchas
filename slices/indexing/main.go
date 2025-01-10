1127c96c187c3bc9cd617dd783ec7dec2a9bce2b
yesyouken.space <16360559+YesYouKenSpace@users.noreply.github.com>
2025-01-10 16:25:26 +0800

    test out indexing

---
+++ slices/indexing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Printf("s=%+v\n", s)
+	// This will print an empty slice
+	fmt.Printf("s[0:0]=%+v\n", s[0:0])
+	fmt.Printf("s[0:5]=%+v\n", s[0:5])
+	// This will print [1]
+	fmt.Printf("s[0:1]=%+v\n", s[0:1])
+	// Printing in reverse order is not allowed
+	// fmt.Printf("s[len(s)-2:len(s)-3]=%+v\n", s[len(s)-2:len(s)-3])
+}
